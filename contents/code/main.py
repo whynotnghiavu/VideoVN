be2c49f20a7b588c2adfdcd1e73d707403cd20f3
whynotnghiavu <176358444+whynotnghiavu@users.noreply.github.com>
2024-08-27 02:12:12 +0700

    create .gitignore, a.py, output.log and main.py

---
+++ contents/code/main.py
@@ -0,0 +1,16 @@
+from modules.MyLog import MyLog
+from view.app import app
+
+
+if __name__ == "__main__":
+    MyLog.title("Bắt đầu chương trình")
+    app()
+# NOTE: Ghi chú bên dưới thay cho README.md
+
+
+#! 0375942842
+
+
+# !
+#! Bug: Đường dẫn đặc biệt khi dùng các hàm ban đầu
+#  file sub có lỗi es???????? (Là lỗi gì????)
