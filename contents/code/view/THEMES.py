a4c72c3243ce7b12c145c7adf84bca31a615badf
whynotnghiavu <176358444+whynotnghiavu@users.noreply.github.com>
2024-09-01 05:38:12 +0700

    update THEMES.py

--- contents/code/view/THEMES.py
+++ contents/code/view/THEMES.py
@@ -5,3 +5,4 @@ class THEMES:
     TAT_CA = ("#0f4c81", "#ffffff")
     DAC_BIET = ("#abd200", "#000000")
     EXIT = ("#800000", "#d4af37")
+# chỉnh  json {background , foreground} tránh 0,1  list []
