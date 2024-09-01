a5ab21a0f66c7418001f5c60ed562604d7334ab4
whynotnghiavu <176358444+whynotnghiavu@users.noreply.github.com>
2024-09-01 12:30:32 +0700

    update main.py

--- contents/code/main.py
+++ contents/code/main.py
@@ -7,14 +7,18 @@ if __name__ == "__main__":
     app()
 # NOTE: Ghi chú bên dưới thay cho README.md
 
+
 # C:\Users\vvn20206205\Downloads\video\DoThiJava
 # C:\Users\vvn20206205\Downloads\video\Jenkins
 # C:\Users\vvn20206205\Downloads\video\SQL\LinkedIn_SQL_Practice_Intermediate_Queries_2023-9_Downloadly.ir\LinkedIn - SQL Practice Intermediate Queries 2023-9
 
+
 # ! Vẫn cần đưa về 1 folder để tạo sub nhanh
 # như nestjs...
- 
+
+
 #! Bug: Đường dẫn đặc biệt khi dùng các hàm ban đầu
 # FileNotFoundError: [WinError 3] The system cannot find the path specified:
+
 
 #!  file sub có lỗi es???????? (Là lỗi gì????)
