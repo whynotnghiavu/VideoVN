66081078f33626ca37ceb0eb9de008f0504d2190
whynotnghiavu <176358444+whynotnghiavu@users.noreply.github.com>
2024-09-01 12:28:40 +0700

    update main.py

--- contents/code/main.py
+++ contents/code/main.py
@@ -8,6 +8,8 @@ if __name__ == "__main__":
 # NOTE: Ghi chú bên dưới thay cho README.md
 
 # C:\Users\vvn20206205\Downloads\video\DoThiJava
+# C:\Users\vvn20206205\Downloads\video\Jenkins
+# C:\Users\vvn20206205\Downloads\video\SQL\LinkedIn_SQL_Practice_Intermediate_Queries_2023-9_Downloadly.ir\LinkedIn - SQL Practice Intermediate Queries 2023-9
 
 # ! Vẫn cần đưa về 1 folder để tạo sub nhanh
 # như nestjs...
