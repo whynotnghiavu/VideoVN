3fc812b3a9dcf7365eabdfc2ed97ed485f4a7571
whynotnghiavu <176358444+whynotnghiavu@users.noreply.github.com>
2024-09-03 00:26:56 +0700

    update 5 files

--- contents/code/main.py
+++ contents/code/main.py
@@ -13,8 +13,18 @@ if __name__ == "__main__":
 # C:\Users\vvn20206205\Downloads\video\SQL\LinkedIn_SQL_Practice_Intermediate_Queries_2023-9_Downloadly.ir\LinkedIn - SQL Practice Intermediate Queries 2023-9
 
 
+# C:\Users\vvn20206205\Downloads\video\auth\LinkedIn - Web Security User Authentication and Access Control 2023-10
+# C:\Users\vvn20206205\Downloads\video\git      \22222222222
+# C:\Users\vvn20206205\Downloads\video\ssl\LinkedIn - SSLTLS for Beginners Securing Network Communications (2019) 2023-8
+
+
+
+# C:\Users\vvn20206205\Downloads\video\contents\git\LinkedIn - Learning Git and GitHub 2023-10
+
+
+
 # ! Vẫn cần đưa về 1 folder để tạo sub nhanh
-# như nestjs...
+# như nestjs  ...
 
 
 #! Bug: Đường dẫn đặc biệt khi dùng các hàm ban đầu
--- contents/code/modules/ThayTheVanBan.py
+++ contents/code/modules/ThayTheVanBan.py
@@ -3,10 +3,11 @@ def ThayTheVanBan(text):
     # text = text.replace
     # Microsof => Dịch vụ vi mô
     text = text.replace("[Tự động tạo]", "")
+    text = text.replace("[Được tạo tự động]", "")
     text = text.replace("\u202B", "")
-    text = text.replace('\u202B', '')
+     
 
     # !Thay thế văn bản
     while "  " in text:
         text = text.replace("  ", " ")
-    return text
+    return text.strip()
