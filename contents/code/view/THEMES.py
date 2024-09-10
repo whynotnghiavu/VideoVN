8c6dc861759c736039e6d9000aa3bdef7dd28db7
whynotnghiavu <176358444+whynotnghiavu@users.noreply.github.com>
2024-09-10 10:08:47 +0700

    update main.py and THEMES.py

--- contents/code/view/THEMES.py
+++ contents/code/view/THEMES.py
@@ -1,8 +1,13 @@
 class THEMES:
+    # Xanh lá
     DOC_LAP = ("#00d1b2", "#000000")
+    # Cam
     CHON_THU_MUC = ("#ff8f1c", "#000000")
+    # Dracula
     BINH_THUONG = ("#282a36", "#bd93f9")
+    # Xanh đậm
     TAT_CA = ("#0f4c81", "#ffffff")
+    # Vàng
     DAC_BIET = ("#abd200", "#000000")
     EXIT = ("#800000", "#d4af37")
 # chỉnh  json {background , foreground} tránh 0,1  list []
